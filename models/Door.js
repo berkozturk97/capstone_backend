da7fb4579bbfbe172b4891ca0506d98d9553b39f
seckinbrke <seckin_brke@hotmail.com>
2020-10-26 15:30:15 +0300

    new apis

---
+++ models/Door.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DoorSchema = new Schema({
+    doorName: {
+        type: String,
+        required: [true, '`{PATH}` alanı zorunludur.'],
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Door', DoorSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const { db } = require('../models/User');
 const Log = require('../models/Log');
 var router = express.Router();
 
 const UserPackage = require('../models/User');
+const Door = require('../models/Door');
 
 router.post('/addUser', (req, res) => {
     const { fullName, email, rfid, password, permissions } = req.body;
@@ -22,13 +24,13 @@ router.post('/addUser', (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const user = await UserPackage.findByCredentials(req.body.email, req.body.password)
-        res.send({ user})
+        const user = await UserPackage.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
     } catch (error) {
         res.status(400).send()
     }
 })
-
 
 router.post('/hasPermisson', async (req, res) => {
     const { rfid, doorid } = req.body;
@@ -49,7 +51,24 @@ router.post('/hasPermisson', async (req, res) => {
 
 })
 
-router.get('/get', (req, res) => {
+router.post('/getUsersByName', (req, res) => {
+    const { fullName } = req.body;
+    Products.find({ name: { $options: 'i', $regex: fullName } }).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err)
+    })
+})
+
+router.get('/getAllDoor', (req, res) => {
+    Door.find().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err)
+    })
+})
+
+router.get('/getUsers', (req, res) => {
     UserPackage.find().then((data) => {
         res.json(data);
     }).catch((err) => {
@@ -57,4 +76,19 @@ router.get('/get', (req, res) => {
     })
 })
 
+router.put('/updateUserInfo/:id', (req, res) => {
+    //
+    UserPackage.findByIdAndUpdate(req.body._id, {
+        permissions: req.body.permissions,
+        fullName: req.body.fullName,
+        rfid: req.body.rfid
+    }, {
+        multi: true
+    }, (err, data) => {
+        if (err) {
+            res.send(err)
+        }
+        res.json(data)
+    })
+})
 module.exports = router;
