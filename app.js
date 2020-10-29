8ddcde092e151f1013d0e976440ef35bb796a9e5
seckinbrke <seckin_brke@hotmail.com>
2020-10-29 14:40:58 +0300

    hasPermission

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 app.use('/', indexRouter);
 
-app.use('/userPackage', userRouter);
+app.use('/user', userRouter);
 app.use('/door', doorRouter);
 app.use('/log', logRouter);
 
--- routes/user.js
+++ routes/user.js
@@ -50,10 +50,10 @@ router.post('/hasPermisson', async (req, res) => {
     });
 
 })
-
-router.get('/hasPermisson/:rfid/:doorid', async (req, res) => {
+router.get('/hasPermisson/:rfid/:doorId', async (req, res) => {
     const { rfid, doorId } = req.params;
     const user = await UserPackage.find({ rfid: rfid });
+    console.log(user[0])
     let isCanOpen = await user[0].permissions.includes(doorId);
 
     const log = new Log({
